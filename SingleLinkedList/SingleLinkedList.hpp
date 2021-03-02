217ab221178cb6ec643646d93eda6c5da9701b8c
Afroz <afrozahmed441@gmail.com>
2021-03-02 19:13:33 +0530

    Added more operations on Single Linked List

--- SingleLinkedList/SingleLinkedList.hpp
+++ SingleLinkedList/SingleLinkedList.hpp
@@ -1,9 +1,9 @@
 #ifndef SINGLELINKEDLIST_HPP
 #define SINGLELINKEDLIST_HPP
+#include <algorithm>
+#include <vector>
 
-using std::cout;
-using std::cin;
-using std::endl;
+using namespace std;
 
 class SingleLinkedList
 {
@@ -38,6 +38,16 @@ public:
         }
     }
     
+    /** Insertion at tail node; Method*/
+    void insertAtTail(int val){
+        if(head == NULL) head = newNode(val);
+        else {
+            node *temp = head;
+            while(temp->next != NULL) temp = temp->next;
+            temp->next = newNode(val);
+        }
+    }
+    
     /** Delete at head node; Method */
     /** returns the deleted element if list is not empty, else -1 if list is empty*/
     int deleteAtHead() {
@@ -48,6 +58,58 @@ public:
             head = head->next;
         }
       return deletedNode;
+    }
+    
+    /** Delete at tail node; Method*/
+    int deleteAtTail() {
+        int deletedNode {-1};
+        if(head == NULL) cout << "List is Empty" << endl;
+        else {
+            node *temp = head;
+            while(temp->next->next != NULL) temp = temp->next;
+            deletedNode = temp->next->data;
+            temp->next = NULL;
+        }
+        return deletedNode;
+    }
+    
+    /** Find a node in the List, First Occurance in the list; Method*/
+    int findNode(int val) {
+        if(head == NULL) return  -1;
+        else {
+            printList();
+            node *temp = head;
+            while(temp->next != NULL && temp->data != val)  {
+                cout << "   ";
+                temp = temp->next;
+            }
+            if(temp->data == val){
+                cout << "^" << endl;
+                return val;
+            }
+        return -1;
+        }
+    }
+    
+    /** Remove duplicate values in the List; Method*/
+    void removeDuplicate() {
+        if(head == NULL) cout << "List is Empty" << endl;
+        else if(head->next == NULL) cout << "Only one node in List" << endl;
+        else {
+            vector <int> list;
+            node *cur = head;
+            node *prev = NULL;
+            while(cur != NULL) {
+                if(find(list.begin(), list.end(), cur->data) != list.end()){
+                    prev->next = cur->next;
+                }
+                else {
+                    list.push_back(cur->data);
+                    prev = cur;
+                }
+                cur = cur->next;
+            }
+        }
     }
     
     /** Print  Method*/
--- SingleLinkedList/main.cpp
+++ SingleLinkedList/main.cpp
@@ -5,7 +5,7 @@ int main(){
     SingleLinkedList list1;
     int choice;
     while(1){
-        cout << "Enter your Choice: " << endl << "1. insert a node at head" << endl  << "2. delete a node at head"  << endl << "3. print all nodes in the List" << endl <<"4. Exit" << endl << " : ";
+        cout << "Enter your Choice: " << endl << "1. insert a node at head" << endl  << "2. delete a node at head"  << endl << "3. insert a node at tail" << endl <<"4. delete a node at tail" << endl << "5. Search for a node " <<endl <<"6. Remove Duplicate nodes from List" <<endl  << "7. print list nodes" << endl << "8. Exit"<< endl << " : ";
         cin >> choice;
         switch(choice) {
             case 1:
@@ -22,10 +22,37 @@ int main(){
                 cout << "------------------------- Successfully Deleted ------------------------------------" << endl;
                 break;
             case 3:
+                int n;
+                cout << "Enter the node: ";
+                cin >> n;
+                list1.insertAtTail(n);
+                cout << "-------------------------- Successfully Inserted ----------------------------------" << endl;
+                break;
+            case 4:
+                int d;
+                d = list1.deleteAtTail();
+                cout << "Deleted node is: " << d << endl;
+                cout << "------------------------- Successfully Deleted ------------------------------------" << endl;
+                break;
+            case 5:
+                int searchNode, sn;
+                cout << "Enter the node to search: ";
+                cin >> searchNode;
+                sn = list1.findNode(searchNode);
+                if(sn != -1) cout << "--------------------------- Node Found -----------------------------" << endl;
+                else cout << endl << "---------------------- Node not Found -------------------------------------" << endl;
+                break;
+            case 6:
+                list1.removeDuplicate();
+                cout << "List: ";
+                list1.printList();
+                cout << "--------------------------------- Successfully removed Duplicate nodes -------------------------------- " << endl;
+                break;
+            case 7:
                 cout << "------------------------- Printing list ----------------------------------------------" << endl;
                 list1.printList();
                 break;
-            case 4:
+            case 8:
                 exit(0);
                 break;
             default: 
